5ae9b9cc7987f21ce296869770850f5183d25569
Virginia de Paula de Souza Campos <virginia.campos@pixeon.com>
2020-07-29 02:17:03 -0300

    Setup inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div >
+        <Menu />
+
+        <BannerMain
+        videoTitle ={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Racismo institucional, esta é uma ferida maior do que você imagina."}
+        />
+        <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+        />
+        
+
+        <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
